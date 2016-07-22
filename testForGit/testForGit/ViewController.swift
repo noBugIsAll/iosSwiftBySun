2c9a67cb23c53b99495ca79ad37c2bbc288eff3d
demon <demon@demondemac-mini.local>
2016-07-22 13:07:35 +0800

    helloworld

---
+++ testForGit/testForGit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  testForGit
+//
+//  Created by demon on 16/7/22.
+//  Copyright © 2016年 com.demon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("helloworld!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
